7ff798a027b40e4a11c0f10c4b711e3ba637edf9
Pat Smith <pat.a.smith.dev@gmail.com>
2024-02-02 09:19:42 -0500

    added JSDoc style comments

--- javascript/src/regex.js
+++ javascript/src/regex.js
@@ -1,4 +1,8 @@
-// Eliminates all occurences of character regardless of case
+/**
+ * Eliminates all occurrences of character regardless of case
+ * @param {string} string - The string to operate on
+ * @param {string} character - The character to eliminate
+ */
 function eliminateLetter(string, character) {
   // g is the global flag. It is necessary to match all instances of the character in the string.
   // i is the ignore case flag. It matches both lowercase and uppercase versions of a character.
@@ -6,6 +10,11 @@ function eliminateLetter(string, character) {
   return string.replace(re, "");
 }
 
+/**
+ * Eliminates all occurrences of multiple characters regardless of case
+ * @param {string} string - The string to operate on
+ * @param {...string} characters - The characters to eliminate
+ */
 function eliminateLetters(string, ...characters) {
   // Use the spread operator to catch all parameters passed to this function beyond the first.
   let re = new RegExp("[" + characters.join("") + "]", 'gi');
