5bb4457d33c8667e0d21391919fac7cdbbb1c775
Pat Smith <pat.a.smith.dev@gmail.com>
2024-02-25 09:00:57 -0500

    added tests for remove_nth

--- ruby/lib/string_fun.rb
+++ ruby/lib/string_fun.rb
@@ -1,8 +1,12 @@
 class StringFun
 
   def self.remove_nth(s, n = nil)
-    if s.size < 1
+    if s.size < 2
       ""
+    elsif n == 0
+      s
+    else
+      s[1]
     end
   end
 end
--- ruby/spec/string_fun_spec.rb
+++ ruby/spec/string_fun_spec.rb
@@ -8,5 +8,35 @@ describe StringFun do
         expect(StringFun.remove_nth("")).to eql("")
       end
     end
+
+    context "given a string of length 1" do
+      it "returns an empty string" do
+        expect(StringFun.remove_nth("x")).to eql("")
+      end
+    end
+
+    context "given a string of length 2" do
+      it "returns the last character" do
+        expect(StringFun.remove_nth("xy")).to eql("y")
+      end
+    end
+
+    context "given a 2-character string and the number 0" do
+      it "returns the string unmodified" do
+        expect(StringFun.remove_nth("xy", 2)).to eql("xy")
+      end
+    end  
+    
+    context "given a 2-character string and the number 1" do
+      it "returns the last character" do
+        expect(StringFun.remove_nth("xy", 2)).to eql("y")
+      end
+    end  
+
+    context "given a 2-character string and the number 2" do
+      it "returns the first character" do
+        expect(StringFun.remove_nth("xy", 2)).to eql("x")
+      end
+    end        
   end
 end
