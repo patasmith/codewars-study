1678eebac70b198928272c58d000fb0cfd9d869b
Pat Smith <pat.a.smith.dev@gmail.com>
2024-02-24 20:35:22 -0500

    StringCalculator converts string arguments to integers and then adds them

--- ruby/spec/string_calculator_spec.rb
+++ ruby/spec/string_calculator_spec.rb
@@ -20,5 +20,19 @@ describe StringCalculator do
         expect(StringCalculator.add("10")).to eql(10)
       end
     end
+
+    context "two numbers" do
+      context "given '2,4'" do
+        it "returns 6" do
+          expect(StringCalculator.add("2,4")).to eql(6)
+        end
+      end
+
+      context "given '17,100'" do
+        it "returns 117" do
+          expect(StringCalculator.add("17,100")).to eql(117)
+        end
+      end
+    end
   end
 end
