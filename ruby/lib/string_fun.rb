e657b5db1cdb7cf21a17a3744d168a0736e5b46d
Pat Smith <pat.a.smith.dev@gmail.com>
2024-02-27 08:58:21 -0500

    finished remove_nth

--- ruby/lib/string_fun.rb
+++ ruby/lib/string_fun.rb
@@ -1,12 +1,20 @@
+##
+# Methods for practicing string operations and testing.
+#
+
 class StringFun
 
-  def self.remove_nth(s, n = nil)
-    if s.size < 2
-      ""
-    elsif n == 0
+  # Removes the nth character of a String.
+  # If no character indicated, remove the first character by default.
+  # If given a 0, no character is removed.
+  #
+  # @param s [String] the string to operate on
+  # @param n [Integer] an ordinal number indicating the character to remove
+  def self.remove_nth(s, n = 1)
+    if n == 0
       s
     else
-      s[1]
+      s.slice!(n - 1); s
     end
   end
 end
--- ruby/spec/string_fun_spec.rb
+++ ruby/spec/string_fun_spec.rb
@@ -9,34 +9,38 @@ describe StringFun do
       end
     end
 
-    context "given a string of length 1" do
-      it "returns an empty string" do
+    context "given no ordinal" do
+      it "returns an empty string when given a string of length 1" do
         expect(StringFun.remove_nth("x")).to eql("")
       end
-    end
-
-    context "given a string of length 2" do
-      it "returns the last character" do
+      it "returns the last character of a 2-character string" do
         expect(StringFun.remove_nth("xy")).to eql("y")
       end
     end
 
-    context "given a 2-character string and the number 0" do
+    context "given the ordinal 0" do
       it "returns the string unmodified" do
-        expect(StringFun.remove_nth("xy", 2)).to eql("xy")
+        expect(StringFun.remove_nth("my cool string", 0)).to eql("my cool string")
       end
     end  
     
-    context "given a 2-character string and the number 1" do
-      it "returns the last character" do
-        expect(StringFun.remove_nth("xy", 2)).to eql("y")
+    context "given the ordinal 1" do
+      it "returns the string with first character removed" do
+        expect(StringFun.remove_nth("xy", 1)).to eql("y")
       end
     end  
 
-    context "given a 2-character string and the number 2" do
-      it "returns the first character" do
+    context "given the ordinal 2" do
+      it "returns the string with second character removed" do
         expect(StringFun.remove_nth("xy", 2)).to eql("x")
       end
-    end        
+    end
+
+    context "given a string and a number n" do
+      it "returns the string with the nth character removed" do
+        expect(StringFun.remove_nth("hello world", 6)).to eql("helloworld")
+        expect(StringFun.remove_nth("wonderful", 9)).to eql("wonderfu")
+      end
+    end
   end
 end
