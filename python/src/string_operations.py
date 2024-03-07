4d97fe21d49cd09082cf19f26ef48a40303f8f3d
Pat Smith <pat.a.smith.dev@gmail.com>
2024-03-07 09:02:02 -0500

    making a Numeral class for is_roman (python)

--- python/src/string_operations.py
+++ python/src/string_operations.py
@@ -1,4 +1,8 @@
-def fun_mask(string, pattern="#"):
+def fun_mask(string: str, pattern: str = "#") -> str:
+    """Mask all but the last 4 characters of a string
+    by repeating the given pattern.
+    """
+
     length = len(string)
     if length < 4:
         return string
@@ -10,5 +14,34 @@ def fun_mask(string, pattern="#"):
         return result + string[-4:]
 
 
-def is_roman(string):
+def is_roman(string: str) -> bool:
+    """Roman numeral validator.
+    See https://www.thevbprogrammer.com/Ch08/08-10-RomanNumerals.htm for rules.
+    """
+
+    class Numeral:
+        """A roman numeral."""
+
+        def __init__(
+            self,
+            letter: str,
+            value: int,
+            appear_once: bool,
+            subtractor: bool,
+        ):
+            self.letter = letter
+            self.value = value
+            self.appear_once = appear_once
+            self.subtractor = subtractor
+
+    numerals = [
+        Numeral("M", 1000, False, False),
+        Numeral("D", 500, True, False),
+        Numeral("C", 100, False, True),
+        Numeral("L", 50, True, False),
+        Numeral("X", 10, False, True),
+        Numeral("V", 5, True, False),
+        Numeral("I", 1, False, True),
+    ]
+
     return False
